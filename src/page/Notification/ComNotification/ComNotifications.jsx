ec04b41a07ee347f93c9ed5638bc8a4930543568
Nguyen Tien Tai <capterfinn123@gmail.com>
2024-08-06 15:02:40 +0700

    Fix: Ui history service

--- src/page/Notification/ComNotification/ComNotifications.jsx
+++ src/page/Notification/ComNotification/ComNotifications.jsx
@@ -5,54 +5,61 @@ import { useNavigation } from "@react-navigation/native";
 
 export default function ComNotification({ tile, data }) {
   const navigation = useNavigation();
+
   const formatStatus = (status) => {
     if (status === "Completed") {
       return "Đã hoàn thành";
     }
+    return status; // Return the original status if not "Completed"
   };
 
   return (
     <View style={styles.main}>
       <ComTitle style={{ fontSize: 24, marginBottom: 10 }}>{tile}</ComTitle>
       <View style={styles.contex}>
-        {data?.map((value, index) => (
-          <View
-            key={index}
-            style={[index !== data.length - 1 && styles.bodySeparator]}
-          >
-            <TouchableOpacity
-              style={[styles.body]}
-              onPress={() => {
-                navigation.navigate("DeliveryDetail", {
-                  id: value.id,
-                  orderId: value.order.id,
-                });
-              }}
+        {data?.length > 0 ? (
+          data.map((value, index) => (
+            <View
+              key={index}
+              style={[index !== data.length - 1 && styles.bodySeparator]}
             >
-              <View style={styles?.container}>
-                <Text
-                  numberOfLines={1}
-                  style={{ fontWeight: "bold", fontSize: 14 }}
-                >
-                  Mã đơn hàng: {value?.order?.invoiceCode}
-                </Text>
-                <Text
-                  numberOfLines={1}
-                  style={{ fontWeight: "bold", fontSize: 14 }}
-                >
-                  Tên khách hàng: {value?.address?.account?.fullName}
-                </Text>
-                <Text numberOfLines={1}>
-                  Ngày giao: {new Date(value?.createDate).toLocaleDateString()}
-                </Text>
-                <Text numberOfLines={1}>
-                  Trạng thái: {formatStatus(value?.status)}
-                </Text>
-                <Text style={{ color: "#0d6bde" }}>Xem chi tiết</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        ))}
+              <TouchableOpacity
+                style={[styles.body]}
+                onPress={() => {
+                  navigation.navigate("DeliveryDetail", {
+                    id: value.id,
+                    orderId: value.order.id,
+                  });
+                }}
+              >
+                <View style={styles?.container}>
+                  <Text
+                    numberOfLines={1}
+                    style={{ fontWeight: "bold", fontSize: 14 }}
+                  >
+                    Mã đơn hàng: {value?.order?.invoiceCode}
+                  </Text>
+                  <Text
+                    numberOfLines={1}
+                    style={{ fontWeight: "bold", fontSize: 14 }}
+                  >
+                    Tên khách hàng: {value?.address?.account?.fullName}
+                  </Text>
+                  <Text numberOfLines={1}>
+                    Ngày giao:{" "}
+                    {new Date(value?.createDate).toLocaleDateString()}
+                  </Text>
+                  <Text numberOfLines={1}>
+                    Trạng thái: {formatStatus(value?.status)}
+                  </Text>
+                  <Text style={{ color: "#0d6bde" }}>Xem chi tiết</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          ))
+        ) : (
+          <Text style={styles.noTaskText}>Chưa có nhiệm vụ hoàn thành</Text>
+        )}
       </View>
     </View>
   );
@@ -82,5 +89,12 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 1,
     borderColor: "#000",
+    padding: 10,
+  },
+  noTaskText: {
+    textAlign: "center",
+    fontSize: 16,
+    color: "#888",
+    marginVertical: 20,
   },
 });
